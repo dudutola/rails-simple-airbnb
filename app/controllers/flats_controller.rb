9e128539043d61c51e97720bfb2205d596181241
Dulcelene Machado <dudutola@gmail.com>
2025-02-26 15:33:43 +0000

    As a user, I can add a flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -7,9 +7,27 @@ class FlatsController < ApplicationController
 
   def show; end
 
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_flat
     @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
