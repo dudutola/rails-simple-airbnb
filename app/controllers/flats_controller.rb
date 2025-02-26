e986f85d72ebd336efe73b2a4f855c8088fc394a
Dulcelene Machado <dudutola@gmail.com>
2025-02-26 16:00:28 +0000

    As a user, I can delete a flat from the website

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,5 +1,5 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: [ :show, :edit, :update ]
+  before_action :set_flat, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @flats = Flat.all
@@ -29,6 +29,11 @@ class FlatsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to @flat, status: :see_other
   end
 
   private
